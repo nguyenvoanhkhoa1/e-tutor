826bc5926057064d60e37345de38ca03cbf4cd9f
nguyenvoanhkhoa1 <55738921+nguyenvoanhkhoa1@users.noreply.github.com>
2024-11-04 01:19:22 +0700

    update tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
-import type { Config } from "tailwindcss";
+import { error } from "console"
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -11,9 +12,76 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        grayScale: {
+          50: "#F5F7FA",
+          100: "#E9EAF0",
+          200: "#CED1D9",
+          300: "#B7BAC7",
+          400: "#A1A5B3",
+          500: "#8C94A3",
+          600: "#6E7485",
+          700: "#4E5566",
+          800: "#363B47",
+          900: "#1D2026",
+        },
+        primary: {
+          100: "#FFEEE8",
+          200: "#FFDDD1",
+          300: "#FFA386",
+          400: "#FF855E",
+          500: "#FF6636",
+          600: "#CC522B",
+          700: "#993D20",
+          800: "#662916",
+          900: "#33140B",
+        },
+        secondary: {
+          100: "#EBEBFF",
+          200: "#CDCBFE",
+          300: "#9A95FE",
+          400: "#7872FD",
+          500: "#564FFD",
+          600: "#453FCA",
+          700: "#342F98",
+          800: "#222065",
+          900: "#111033",
+        },
+        success: {
+          100: "#E1F7E3",
+          200: "#C3E5C6",
+          300: "#7BD785",
+          400: "#4FCA5C",
+          500: "#23BD33",
+          600: "#1C9729",
+          700: "#15711F",
+          800: "#0E4C14",
+          900: "#07260A",
+        },
+        warning: {
+          100: "#FFF2E5",
+          200: "#FED1A5",
+          300: "#FEBB79",
+          400: "#FDA44C",
+          500: "#FD8E1F",
+          600: "#CC7319",
+          700: "#985613",
+          800: "#65390C",
+          900: "#331D06",
+        },
+        error: {
+          100: "#FFF0F0",
+          200: "#F4C8C8",
+          300: "#EE8F8F",
+          400: "#E96969",
+          500: "#E34444",
+          600: "#B63636",
+          700: "#882929",
+          800: "#5B1B1B",
+          900: "#2D0E0E",
+        },
       },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
